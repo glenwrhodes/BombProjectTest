c5dfbcdf9148b1ce734b4a45c80e39d5d1b3a369
Glen Rhodes <glenwrhodes@gmail.com>
2018-01-07 13:40:24 -0500

    - Initial Commit of Unreal Engine 4.18 project with sample content assets to begin creating prefabs.

---
+++ Source/BombermanTest/BombermanTestGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BombermanTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOMBERMANTEST_API ABombermanTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
